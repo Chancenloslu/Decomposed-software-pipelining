2c2a5948c65baeb8fd473e68f9372fb591aa3570
chris <lu.chaorancn@126.com>
2023-02-19 22:42:25 +0100

    create DSP class and add some notes

---
+++ scheduler-framework/src/scheduler/DSP.java
@@ -0,0 +1,15 @@
+package scheduler;
+
+/**
+ * class to implement of Decomposed software pipelining
+ *
+ *  1. create LDDG  <== Graph class but without weight after initialization
+ *                      edge info contained in Node.successor, Node.predecessor
+ *  2. using list scheduler to perfect LDDG
+ */
+public class DSP {
+
+    public void schedule(final Graph sg) {
+
+    }
+}
